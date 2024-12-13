345e298457ad8e1a6549663f449e60a9b50bf87d
Mason <mason@hicksm.dev>
2024-12-13 14:29:26 -0500

    Implemented PasswordHasher::hashPassword(String, String) #1

--- src/main/java/dev/hicksm/climbing/util/PasswordHasher.java
+++ src/main/java/dev/hicksm/climbing/util/PasswordHasher.java
@@ -1,18 +1,58 @@
 package dev.hicksm.climbing.util;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PasswordHasher {
+	
+	private static final String HASH_ALGORITHM = "SHA-256";
 	
 	public static String generateSalt() {
 		// TODO: Implement stub
 		return null;
 	}
 	
-	public static HashedPassword hashPassword(String rawPassword, String salt) {
-		// TODO: Implement stub
-		return null;
+	/// from https://www.baeldung.com/sha-256-hashing-java
+	private static String bytesToHex(byte[] hash) {
+	    StringBuilder hexString = new StringBuilder(2 * hash.length);
+	    for (int i = 0; i < hash.length; i++) {
+	        String hex = Integer.toHexString(0xff & hash[i]);
+	        if(hex.length() == 1) {
+	            hexString.append('0');
+	        }
+	        hexString.append(hex);
+	    }
+	    return hexString.toString();
 	}
 	
-	public class HashedPassword {
+	public static HashedPassword hashPassword(String rawPassword, String salt) 
+			throws IllegalArgumentException {
+		HashedPassword res = new HashedPassword();
+		
+		// Pre-process password and salt
+		if (rawPassword == null)
+			throw new IllegalArgumentException("Raw password cannot be null.");
+		if (salt == null)
+			salt = "";
+		String s = salt + rawPassword;
+		
+		// Retrieve SHA-256 digest instance and hash string to UTF-8 bytes
+		MessageDigest d;
+		try {
+			d = MessageDigest.getInstance(HASH_ALGORITHM);
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException(
+					"PasswordHasher tried to use invalid hashing algorithm " + HASH_ALGORITHM);
+		}
+		byte[] encoded = d.digest(s.getBytes(StandardCharsets.UTF_8));
+		
+		res.hashedPassword = bytesToHex(encoded);
+		
+		return res;
+	}
+	
+	public static class HashedPassword {
 		
 		public String salt;
 		
---
+++ src/test/java/dev/hicksm/climbing/util/PasswordHasherTest.java
@@ -0,0 +1,104 @@
+package dev.hicksm.climbing.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import dev.hicksm.climbing.util.PasswordHasher.HashedPassword;
+
+public class PasswordHasherTest {
+	
+	/*
+		This test checks that, without a salt, a password will be hashed.
+	*/
+	@Test
+	void testHashPassword1() {
+		String s = "donnahaditcoming";
+		
+		HashedPassword res = PasswordHasher.hashPassword(s, null);
+		
+		Assertions.assertNotEquals(s, res.hashedPassword);
+	}
+	
+	/*
+		This test checks that two passwords are hashed to separate values.
+	*/
+	@Test
+	void testHashPassword2() {
+		String s1 = "donnahaditcoming", s2 = "iloveyou";
+		
+		HashedPassword r1 = PasswordHasher.hashPassword(s1, null), 
+				r2 = PasswordHasher.hashPassword(s2, null);
+		
+		Assertions.assertNotEquals(r1.hashedPassword, r2.hashedPassword);
+	}
+	
+	/*
+		This test checks that, given a different salt, the same password 
+		will be hashed to two different values.
+	*/
+	@Test
+	void testHashPassword3() {
+		String s = "donnahaditcoming";
+		String salt1 = "o78chasd", salt2 = "89c7v ";
+		
+		HashedPassword r1 = PasswordHasher.hashPassword(s, salt1),
+				r2 = PasswordHasher.hashPassword(s, salt2);
+		
+		Assertions.assertNotEquals(r1.hashedPassword, r2.hashedPassword);
+	}
+	
+	/*
+		This test checks that, given the same salt, the same password will 
+		be hashed the same way.
+	*/
+	@Test
+	void testHashPassword4() {
+		String s = "donnahaditcoming", salt = "98vdfsa";
+		
+		HashedPassword r1 = PasswordHasher.hashPassword(s, salt),
+				r2 = PasswordHasher.hashPassword(s, salt);
+		
+		Assertions.assertEquals(r1.hashedPassword, r2.hashedPassword);
+	}
+	
+	/*
+		This test checks that hashing with a null password is the same as 
+		hashing with an empty string.
+	*/
+	@Test
+	void testHashPassword5() {
+		String s = "donnahaditcoming";
+		
+		HashedPassword r1 = PasswordHasher.hashPassword(s, null),
+				r2 = PasswordHasher.hashPassword(s, "");
+		
+		Assertions.assertEquals(r1.hashedPassword, r2.hashedPassword);
+	}
+	
+	/*
+		This test verifies that a null password cannot be passed to the hasher.
+	*/
+	@Test
+	void testHashPassword6() {
+		try {
+			PasswordHasher.hashPassword(null, null);
+			Assertions.fail("Password hasher allowed null password");
+		} catch (IllegalArgumentException e) {}
+	}
+	
+	/*
+		This test verifies that two different passwords with two different salts 
+		will hash to different values.
+	*/
+	@Test
+	void testHashPassword7() {
+		String s1 = "donnahaditcoming", s2 = "iloveyou";
+		String salt1 = "8790sad", salt2 = "v6as834d";
+		
+		HashedPassword r1 = PasswordHasher.hashPassword(s1, salt1),
+				r2 = PasswordHasher.hashPassword(s2, salt2);
+		
+		Assertions.assertNotEquals(r1.hashedPassword, r2.hashedPassword);
+	}
+
+}
